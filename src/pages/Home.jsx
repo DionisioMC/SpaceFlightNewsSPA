d03215945e834658cbf464243c45b1ebe241fb2d
Dionísio Coelho <dionisiomeireles@gmail.com>
2025-01-16 23:25:52 +0000

    Continue to add to the Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,11 @@ function Home() {
     return(
         <>
             <h2>Home Page</h2>
+            
+            <div id="quotes">
+                <q>The earth is the cradle of humanity , but mankind cannot stay in the cradle forever.</q>
+            </div>
+            
         </>
     )
 }
