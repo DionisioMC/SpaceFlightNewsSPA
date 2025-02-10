0f5dfd21dacbb86c2d433ce8bd5996304a942850
Dionísio Coelho <dionisiomeireles@gmail.com>
2025-02-10 18:48:36 +0000

    Adding more quotes and spacing them out

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,6 +5,8 @@ function Home() {
             
             <div id="quotes">
                 <q>The earth is the cradle of humanity , but mankind cannot stay in the cradle forever.</q>
+                <q>We are just an advanced breed of monkeys on a minor planet of a very average star. But we can understand the Universe. That makes us something very special.</q>
+                <q>Who among us has never looked up into the heavens on a starlit night, lost in wonder at the vastness of space and the beauty of the stars?</q>
             </div>
             
         </>
