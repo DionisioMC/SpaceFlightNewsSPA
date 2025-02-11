ef70982a580f7da8d8cead824dffb92a0b3c2e09
Dionísio Coelho <dionisiomeireles@gmail.com>
2025-02-11 14:29:57 +0000

    Editing the about page and the home page to be more visually appealing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
 function About() {
     return(
         <>
-            <h1>Hello Again</h1>
+            <div className="body">
+                <h2>About</h2>
+
+                <p>This Single page application was developed using React, Vite and the Space Flight News API. The code for this project can be consulted <a href="https://github.com/DionisioMC/SpaceFlightNewsSPA">here</a></p>
+            </div>
+            
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 function Home() {
     return(
         <>
-            <h2>Home Page</h2>
+            <div className="body">
+                <h2>Home Page</h2>
             
-            <div id="quotes">
-                <p><q>The earth is the cradle of humanity , but mankind cannot stay in the cradle forever.</q> - Konstantin Tsiolkovsky</p>
-                <p><q>We are just an advanced breed of monkeys on a minor planet of a very average star. But we can understand the Universe. That makes us something very special.</q> - Stephen Hawking</p>
-                <p><q>Who among us has never looked up into the heavens on a starlit night, lost in wonder at the vastness of space and the beauty of the stars?</q> - Jeb Bush</p>
+                <div id="quotes">
+                    <p><q>The earth is the cradle of humanity , but mankind cannot stay in the cradle forever.</q> - Konstantin Tsiolkovsky</p>
+                    <p><q>We are just an advanced breed of monkeys on a minor planet of a very average star. But we can understand the Universe. That makes us something very special.</q> - Stephen Hawking</p>
+                    <p><q>Who among us has never looked up into the heavens on a starlit night, lost in wonder at the vastness of space and the beauty of the stars?</q> - Jeb Bush</p>
+                </div>
             </div>
+            
             
         </>
     )
